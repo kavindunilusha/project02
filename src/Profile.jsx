5e7a9bad14670c1630341fbc5755e768f54f5f72
Kavindu Galgamuwa <125373634+kavindunilusha@users.noreply.github.com>
2024-02-10 10:24:18 +0530

    dlete one row

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -16,16 +16,11 @@ function Profile() {
       <tbody>
         <tr>
           <td>1</td>
-          <td>Mark</td>
+
           <td>Otto</td>
           <td>@mdo</td>
         </tr>
-        <tr>
-          <td>2</td>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
+
         <tr>
           <td>3</td>
           <td colSpan={2}>Larry the Bird</td>
